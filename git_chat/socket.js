45467f183efa89f6dd3bd70cf1bc86a37a44512f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-11-12 12:22:10 +0900

    fix :: 소켓이벤트 연결

--- git_chat/socket.js
+++ git_chat/socket.js
@@ -1,24 +1,27 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app) => {
   const io = SocketIO(server, { path: '/socket.io' });
+  app.set('io', io);
+  const room = io.of('/room');
+  const chat = io.of('/chat');
 
-  io.on('connection', (socket) => { 
-    const req = socket.request;
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
-    socket.on('disconnect', () => { 
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
+    socket.on('disconnect', () => {
+      console.log('room 네임스페이스 접속 해제');
     });
-    socket.on('error', (error) => { 
-      console.error(error);
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스에 접속');
+
+    socket.on('join', (data) => { 
+      socket.join(data); 
     });
-    socket.on('reply', (data) => { 
-      console.log(data);
+
+    socket.on('disconnect', () => {
+      console.log('chat 네임스페이스 접속 해제');
     });
-    socket.interval = setInterval(() => { 
-      socket.emit('news', 'Hello Socket.IO');
-    }, 3000);
   });
 };
